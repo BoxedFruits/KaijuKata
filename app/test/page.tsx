f9210f08af8eaf2eb899112352f240bc5be10bc6
BoxedFruits <samballesteros98@gmail.com>
2023-06-03 16:14:58 -0400

    radios as tabs

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,12 @@
 "use client"
+import * as test from "../../data/test.json"
 
 import Editor, { Monaco, DiffEditor } from "@monaco-editor/react"
 import { type editor } from 'monaco-editor';
 import { useRef, useState } from "react"
 
 export default function Page() {
+  console.log(test.lessonName[0])
   const [currentInput, setCurrentInput] = useState("")
   const goal = "pragma solidity ^0.8.0;"
   const monacoRef = useRef<Monaco | null>(null);
@@ -15,35 +17,43 @@ export default function Page() {
   }
 
   function handleEditorDidMount(editor: editor.IStandaloneCodeEditor, monaco: Monaco) {
-    // here is another way to get monaco instance
-    // you can also store it in `useRef` for further usage
-    console.log(typeof editor)
     editorRef.current = editor;
     monacoRef.current = monaco;
-    console.log(monaco.languages.typescript.javascriptDefaults.getExtraLibs())
   }
 
   return (
     <div>
+      <div className="flex mx-3 my-2">
+        {
+          test.lessonName.map((value, index) => {
+            return (
+              <div className="flex mr-8">
+                <label for={`tab-${index}`}>{value.tabName}</label>
+                <input type="radio" name="tabs" id={`tab-${index}`}/>
+              </div>
+            )
+          })}
+      </div>
       <Editor
+        defaultValue="blahblah"
         height="80vh"
         width="100vw"
         language="sol"
         beforeMount={handleEditorWillMount}
         onMount={handleEditorDidMount}
-        options={{minimap: {enabled: false}}}
+        options={{ minimap: { enabled: false } }}
       />
       <DiffEditor
-        original={currentInput !== "" ? currentInput : "" }
-        modified={currentInput !== "" ? goal : "" }
+        original={currentInput !== "" ? currentInput : ""}
+        modified={currentInput !== "" ? goal : ""}
         height="15vh"
         width="100%"
         language="sol"
         // beforeMount={handleEditorWillMount}
         // onMount={handleEditorDidMount}
-        options={{ renderSideBySide: false , minimap: {enabled: false}}}
+        options={{ renderSideBySide: false, minimap: { enabled: false } }}
       />
-      <button onClick={() => {setCurrentInput(editorRef.current.getValue())} }>Click me</button>
+      <button onClick={() => { setCurrentInput(editorRef.current.getValue()) }}>Click me</button>
     </div>
   )
 }
