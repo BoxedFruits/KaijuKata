2d8d446648b547eb463781f97d2eca872826b8e7
BoxedFruits <samballesteros98@gmail.com>
2023-06-03 16:35:15 -0400

    change test based on tab clicked

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -8,6 +8,8 @@ import { useRef, useState } from "react"
 export default function Page() {
   console.log(test.lessonName[0])
   const [currentInput, setCurrentInput] = useState("")
+  const [checkedInput, setCheckedInput] = useState(false)
+
   const goal = "pragma solidity ^0.8.0;"
   const monacoRef = useRef<Monaco | null>(null);
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
@@ -21,6 +23,16 @@ export default function Page() {
     monacoRef.current = monaco;
   }
 
+  const handleCheckedInput = () => {
+    setCheckedInput(true)
+    setCurrentInput(editorRef.current.getValue())
+  }
+
+  const handleTabChange = (newText: string) => {
+    setCheckedInput(false)
+    setCurrentInput(newText)
+  }
+console.log(currentInput)
   return (
     <div>
       <div className="flex mx-3 my-2">
@@ -29,13 +41,17 @@ export default function Page() {
             return (
               <div className="flex mr-8">
                 <label for={`tab-${index}`}>{value.tabName}</label>
-                <input type="radio" name="tabs" id={`tab-${index}`}/>
+                <input
+                  type="radio"
+                  name="tabs" id={`tab-${index}`}
+                  onClick={() => handleTabChange(value.defaultValue)}
+                />
               </div>
             )
           })}
       </div>
       <Editor
-        defaultValue="blahblah"
+        value={currentInput}
         height="80vh"
         width="100vw"
         language="sol"
@@ -44,16 +60,15 @@ export default function Page() {
         options={{ minimap: { enabled: false } }}
       />
       <DiffEditor
-        original={currentInput !== "" ? currentInput : ""}
-        modified={currentInput !== "" ? goal : ""}
+        original={currentInput !== "" && checkedInput ? currentInput : ""}
+        modified={currentInput !== "" && checkedInput ? goal : ""}
         height="15vh"
         width="100%"
         language="sol"
-        // beforeMount={handleEditorWillMount}
-        // onMount={handleEditorDidMount}
+
         options={{ renderSideBySide: false, minimap: { enabled: false } }}
       />
-      <button onClick={() => { setCurrentInput(editorRef.current.getValue()) }}>Click me</button>
+      <button onClick={handleCheckedInput}>Click me</button>
     </div>
   )
 }
