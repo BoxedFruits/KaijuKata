e97763b2ccccecf4896f8255278253b8ff642859
BoxedFruits <samballesteros98@gmail.com>
2023-06-04 16:47:43 -0400

    start of navbar layout

---
+++ app/test/layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from '../layout_components/navbar'
+
+const LessonLayout = ({
+    children,
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <>
+            <Navbar/>
+                <main>{children}</main>
+        </>
+    )
+}
+
+
+export default LessonLayout;
