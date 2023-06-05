057155acb807aa7065a27e3e75cb0b3ca2779868
BoxedFruits <samballesteros98@gmail.com>
2023-06-04 23:21:18 -0400

    tab styles

--- app/layout_components/editorPanel.tsx
+++ app/layout_components/editorPanel.tsx
@@ -1,5 +1,3 @@
-//break this out into another component
-//page should handle the api call to get the data of the right half if needed, its state
 "use client"
 import * as test from "../../data/test.json" //TODO: this should come from an api
 
@@ -10,54 +8,57 @@ import { useEffect, useId, useRef, useState } from "react"
 
 //TODO: this should be use an api call to get the data that is passed into it from page.tsx
 const EditorPanel = () => {
-    const [currentInput, setCurrentInput] = useState("")
-    const [checkedInput, setCheckedInput] = useState(false)
-    const [correctValue, setCorrectValue] = useState("")
-    const [currentTab, setCurrentTab] = useState(test.lessonName.defaultTab)
-    const [isReadOnly, setIsReadOnly] = useState(false)
-    const toBeSelectedId = useId(); //NOTE: this is only being used as there is a big with nextjs and the checked attribute. https://github.com/vercel/next.js/issues/49499
-  
-    const monacoRef = useRef<Monaco | null>(null);
-    const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
-  
-    useEffect(() => {
-      //@ts-ignore
-      document.querySelector(`[data-id="${toBeSelectedId}"]`).checked = true;
-    }, [toBeSelectedId])
-  
-  
-    function handleEditorWillMount(monaco: Monaco) {
-      monaco.languages.typescript.javascriptDefaults.setEagerModelSync(true);
+  const [currentInput, setCurrentInput] = useState("")
+  const [checkedInput, setCheckedInput] = useState(false)
+  const [correctValue, setCorrectValue] = useState("")
+  const [currentTab, setCurrentTab] = useState(test.lessonName.defaultTab)
+  const [isReadOnly, setIsReadOnly] = useState(false)
+  const toBeSelectedId = useId(); //NOTE: this is only being used as there is a big with nextjs and the checked attribute. https://github.com/vercel/next.js/issues/49499
+
+  const monacoRef = useRef<Monaco | null>(null);
+  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+
+  const selectedTabStyle = "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
+
+  useEffect(() => {
+    //@ts-ignore
+    document.querySelector(`[data-id="${toBeSelectedId}"]`).checked = true;
+  }, [toBeSelectedId])
+
+
+  function handleEditorWillMount(monaco: Monaco) {
+    monaco.languages.typescript.javascriptDefaults.setEagerModelSync(true);
+  }
+
+  function handleEditorDidMount(editor: editor.IStandaloneCodeEditor, monaco: Monaco) {
+    editorRef.current = editor;
+    monacoRef.current = monaco;
+  }
+
+  const handleCheckedInput = () => {
+    if (isReadOnly !== true) {
+      setCheckedInput(true)
+      setCurrentInput(editorRef!.current!.getValue())
     }
-  
-    function handleEditorDidMount(editor: editor.IStandaloneCodeEditor, monaco: Monaco) {
-      editorRef.current = editor;
-      monacoRef.current = monaco;
-    }
-  
-    const handleCheckedInput = () => {
-      if (isReadOnly !== true) {
-        setCheckedInput(true)
-        setCurrentInput(editorRef!.current!.getValue())
-      }
-    }
-  
-    const handleTabChange = (newText: string, correctValue: string | undefined, index: number) => {
-      setCheckedInput(false)
-      setCurrentInput(newText)
-      setIsReadOnly(correctValue !== undefined ? false : true)
-      setCurrentTab(index)
-      setCorrectValue(correctValue !== undefined ? correctValue : "")
-    }
-  
-    return (
-      <div>
-        <div className="flex mx-3 my-2">
-          {
-            test.lessonName.tabs.map((value, index) => {
-              return (
-                <div className="flex mr-8" key={`editor-tab-${index}`}>
-                  <label htmlFor={`editor-tab-${index}`}>{value.tabName}
+  }
+
+  const handleTabChange = (newText: string, correctValue: string | undefined, index: number) => {
+    setCheckedInput(false)
+    setCurrentInput(newText)
+    setIsReadOnly(correctValue !== undefined ? false : true)
+    setCurrentTab(index)
+    setCorrectValue(correctValue !== undefined ? correctValue : "")
+  }
+
+  return (
+    <div>
+      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 flex mx-3 my-2">
+        {
+          test.lessonName.tabs.map((value, index) => {
+            return (
+              <ul className="flex" key={`editor-tab-${index}`}>
+                <li className="mr-2">
+                  <label className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${currentTab === index ? selectedTabStyle : ''}`} htmlFor={`editor-tab-${index}`}>{value.tabName}
                     <input
                       type="radio"
                       name="tabs"
@@ -72,31 +73,32 @@ const EditorPanel = () => {
                       )}
                     />
                   </label>
-                </div>
-              )
-            })}
-        </div>
-        <Editor
-          defaultValue={test.lessonName.tabs[test.lessonName.defaultTab].defaultValue}
-          value={currentInput}
-          height="80vh"
-          width="100vw"
-          language="sol"
-          beforeMount={handleEditorWillMount}
-          onMount={handleEditorDidMount}
-          options={{ minimap: { enabled: false }, readOnly: isReadOnly }}
-        />
-        <DiffEditor
-          original={currentInput !== "" && checkedInput ? currentInput : ""}
-          modified={currentInput !== "" && checkedInput ? correctValue : ""}
-          height="12vh"
-          width="100%"
-          language="sol"
-          options={{ renderSideBySide: false, minimap: { enabled: false } }}
-        />
-        <button onClick={handleCheckedInput}>Click me</button>
+                </li>
+              </ul>
+            )
+          })}
       </div>
-    )
+      <Editor
+        defaultValue={test.lessonName.tabs[test.lessonName.defaultTab].defaultValue}
+        value={currentInput}
+        height="80vh"
+        width="100vw"
+        language="sol"
+        beforeMount={handleEditorWillMount}
+        onMount={handleEditorDidMount}
+        options={{ minimap: { enabled: false }, readOnly: isReadOnly }}
+      />
+      <DiffEditor
+        original={currentInput !== "" && checkedInput ? currentInput : ""}
+        modified={currentInput !== "" && checkedInput ? correctValue : ""}
+        height="12vh"
+        width="100%"
+        language="sol"
+        options={{ renderSideBySide: false, minimap: { enabled: false } }}
+      />
+      <button onClick={handleCheckedInput}>Click me</button>
+    </div>
+  )
 }
 
 export default EditorPanel;
