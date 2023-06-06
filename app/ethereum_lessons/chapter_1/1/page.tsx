20d07477c0be442caf8cfb76f88474ec26ce0822
BoxedFruits <samballesteros98@gmail.com>
2023-06-05 22:13:36 -0400

    move test page out of test dir

--- app/ethereum_lessons/chapter_1/1/page.tsx
+++ app/ethereum_lessons/chapter_1/1/page.tsx
@@ -1,7 +1,20 @@
+import LayoutButtons from "@/app/layout_components/layoutButtons"
+import EditorPanel from "../../../layout_components/editorPanel"
+import TwoPanelLayout from "../../../layout_components/twoPanelLayout"
+
 export default function Page() {
+  //TODO: make api call and pass into editor panel
   return (
-    <div>
-      <h1>Page 1</h1>
-    </div>
+    <TwoPanelLayout>
+      <div className="panelOfLeft">
+        <div className="lesson">
+          <p>something</p>
+        </div>
+        <LayoutButtons
+          prevRoute="/ethereum_lessons/chapter_1"
+          nextRoute="/ethereum_lessons/chapter_1/2" />
+      </div>
+      <EditorPanel />
+    </TwoPanelLayout>
   )
 }
