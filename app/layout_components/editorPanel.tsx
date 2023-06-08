cdd6c7980cd4fd48fd41f697af6e3fb76ff17470
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-06-07 22:12:38 -0400

    seperate tab into its own component

--- app/layout_components/editorPanel.tsx
+++ app/layout_components/editorPanel.tsx
@@ -4,21 +4,31 @@ import * as test from "../../data/test.json" //TODO: this should come from an ap
 import Editor, { Monaco, DiffEditor } from "@monaco-editor/react"
 import { type editor } from 'monaco-editor';
 import { useEffect, useId, useRef, useState } from "react"
+import Tab, { TabInfo } from "./tab";
+
+export interface CurrentTabInfo {
+  language: string,
+  correctValue?: string,
+  index: number,
+  isReadonly: boolean
+}
 
 
 //TODO: this should be use an api call to get the data that is passed into it from page.tsx
 const EditorPanel = () => {
   const [currentInput, setCurrentInput] = useState("")
   const [checkedInput, setCheckedInput] = useState(false)
-  const [correctValue, setCorrectValue] = useState("")
-  const [currentTab, setCurrentTab] = useState(test.lessonName.defaultTab)
-  const [isReadOnly, setIsReadOnly] = useState(false)
+  const [currentTabInfo, setCurrentTabInfo] = useState<CurrentTabInfo>({
+    language: test.lessonName.tabs[test.lessonName.defaultTab].language,
+    index:test.lessonName.defaultTab,
+    correctValue: test.lessonName.tabs[test.lessonName.defaultTab]?.correctValue !== undefined ? test.lessonName.tabs[test.lessonName.defaultTab]?.correctValue : "",
+    isReadonly: test.lessonName.tabs[test.lessonName.defaultTab]?.correctValue !== undefined ? false : true
+  });
+
   const toBeSelectedId = useId(); //NOTE: this is only being used as there is a big with nextjs and the checked attribute. https://github.com/vercel/next.js/issues/49499
 
   const monacoRef = useRef<Monaco | null>(null);
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
-
-  const selectedTabStyle = "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
 
   useEffect(() => {
     //@ts-ignore
@@ -36,45 +46,38 @@ const EditorPanel = () => {
   }
 
   const handleCheckedInput = () => {
-    if (isReadOnly !== true) {
+    if (currentTabInfo.isReadonly !== true) {
       setCheckedInput(true)
       setCurrentInput(editorRef!.current!.getValue())
     }
   }
 
-  const handleTabChange = (newText: string, correctValue: string | undefined, index: number) => {
+  const handleTabChange = (newText: string, correctValue: string | undefined, index: number, language: string) => {
     setCheckedInput(false)
     setCurrentInput(newText)
-    setIsReadOnly(correctValue !== undefined ? false : true)
-    setCurrentTab(index)
-    setCorrectValue(correctValue !== undefined ? correctValue : "")
+    setCurrentTabInfo({
+      language: language,
+      correctValue: correctValue !== undefined ? correctValue : "",
+      index: index,
+      isReadonly: correctValue !== undefined ? false : true
+    })
   }
 
   return (
     <div className="flex flex-col grow ">
       <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 flex mx-3 my-2">
         {
-          test.lessonName.tabs.map((value, index) => {
+          test.lessonName.tabs.map((tabInfo: TabInfo, index) => {
             return (
-              <ul className="flex" key={`editor-tab-${index}`}>
-                <li className="mr-2">
-                  <label className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${currentTab === index ? selectedTabStyle : ''}`} htmlFor={`editor-tab-${index}`}>{value.tabName}
-                    <input
-                      type="radio"
-                      name="tabs"
-                      id={`editor-tab-${index}`}
-                      key={`editor-tab-${index}`}
-                      value={currentTab === index ? "on" : "off"}
-                      data-id={test.lessonName.defaultTab === index ? toBeSelectedId : undefined}
-                      onClick={() => handleTabChange(
-                        value.defaultValue,
-                        value?.correctValue,
-                        index
-                      )}
-                    />
-                  </label>
-                </li>
-              </ul>
+              <Tab 
+                key={`${index}`}
+                index={index}
+                currentTabInfo={currentTabInfo}
+                tabInfo={tabInfo}
+                toBeSelectedId={toBeSelectedId}
+                defaultSelectedTab={test.lessonName.defaultTab}
+                handleTabChange={handleTabChange}
+              />
             )
           })}
       </div>
@@ -82,19 +85,19 @@ const EditorPanel = () => {
         defaultValue={test.lessonName.tabs[test.lessonName.defaultTab].defaultValue}
         value={currentInput}
         height="75%"
-        language="sol"
+        language={currentTabInfo?.language}
         beforeMount={handleEditorWillMount}
         onMount={handleEditorDidMount}
-        options={{ minimap: { enabled: false }, readOnly: isReadOnly }}
+        options={{ minimap: { enabled: false }, readOnly: currentTabInfo.isReadonly }}
       />
       <div>
         <button style={{ border: "2px solid red" }} onClick={handleCheckedInput}>Check Answer</button>
       </div>
       <DiffEditor
         original={currentInput !== "" && checkedInput ? currentInput : ""}
-        modified={currentInput !== "" && checkedInput ? correctValue : ""}
+        modified={currentInput !== "" && checkedInput ? currentTabInfo.correctValue : ""}
         height="10%"
-        language="sol"
+        language={currentTabInfo?.language}
         options={{ renderSideBySide: false, minimap: { enabled: false } }}
       />
     </div>
---
+++ app/layout_components/tab.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import { CurrentTabInfo } from "./editorPanel";
+
+interface TabProps {
+  key: string,
+  index: number,
+  currentTabInfo: CurrentTabInfo,
+  tabInfo: TabInfo,
+  handleTabChange: Function,
+  toBeSelectedId: string,
+  defaultSelectedTab: number
+}
+
+interface LessonInfo { //Type for the API call
+  lessonName: {
+    defaultTab: number,
+    tabs: TabInfo[]
+  }
+}
+
+export interface TabInfo {
+  tabName: string,
+  language: string,
+  defaultValue: string,
+  correctValue?: string
+}
+
+enum Language { // TODO: figure out why this is not working
+  SOL = "sol",
+  JS = "js",
+  TS = "ts",
+  HTML = "html",
+}
+
+const selectedTabStyle = "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
+const getIcon = (language: string) => {
+  switch (language) {
+    case "sol":
+      return <Image src="/file_type_solidity.svg" alt="solidity" height={24} width={24}></Image>
+    case "js":
+      return <Image src="/file_type_js.svg" alt="javascript"></Image>
+    case "ts":
+      return <Image src="/file_type_typescript.svg" alt="typescript"></Image>
+    case "html":
+      return <Image src="/file_type_html.svg" alt="html" height={24} width={24}></Image>
+  }
+}
+
+
+const Tab = (props: TabProps) => {
+  return (
+    <div>
+      <ul className="flex" key={`editor-tab-${props.index}`}>
+        <li className={`flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600  
+        hover:border-gray-300 dark:hover:text-gray-300  
+        ${props.currentTabInfo.index === props.index ? selectedTabStyle : ''}`}>
+        {getIcon(props.tabInfo.language)}
+        <label
+          htmlFor={`editor-tab-${props.index}`}>{props.tabInfo.tabName}.{props.tabInfo.language}
+          <input
+            type="radio"
+            name="tabs"
+            id={`editor-tab-${props.index}`}
+            key={`editor-tab-${props.index}`}
+            value={props.currentTabInfo.index === props.index ? "on" : "off"}
+            data-id={props.defaultSelectedTab === props.index ? props.toBeSelectedId : undefined}
+            onClick={() => props.handleTabChange(
+              props.tabInfo.defaultValue,
+              props.tabInfo?.correctValue,
+              props.index,
+              props.tabInfo.language
+            )}
+          />
+        </label>
+      </li>
+    </ul>
+    </div >
+  )
+}
+
+export default Tab;
