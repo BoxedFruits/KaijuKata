6f1261590fbcc1815b9b3814553512315dffbf0f
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-06-11 14:56:46 -0400

    rename directory

--- app/layout_components/navbar.tsx
+++ app/layout_components/navbar.tsx
@@ -11,12 +11,12 @@ const Navbar = () => {
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">KaijuKata</span>
           </a>
         </div>
-        <div className="flex w-full md:w-auto items-center" id="navbar-default">
+        <div className="flex w-full md:w-auto items-center mr-5" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li className="mr-2">
               <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Connect</a>
             </li>
-            <li>
+            <li className="mr-5">
               <SideMenu />
             </li>
           </ul>
