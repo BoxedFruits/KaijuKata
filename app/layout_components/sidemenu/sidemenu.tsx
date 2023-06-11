ea2170f71597f32aad097cc3edd12bfccf2c8a70
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-06-10 23:21:03 -0400

    change side nav

--- app/layout_components/sidemenu/sidemenu.tsx
+++ app/layout_components/sidemenu/sidemenu.tsx
@@ -1,34 +1,27 @@
 "use client"
+import { useState } from "react"
 import "../sidemenu/sidemenu.css"
 
 
 const SideMenu = () => {
-    //TODO: hamburger button should be position absolute and turn into the exit button when the menu is open
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.querySelector("body").style.marginRight = "250px";
-        // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    }
+  const [isChecked, setIsChecked] = useState(false)
 
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-        document.querySelector("body").style.marginRight = "0";
-        // document.body.style.backgroundColor = "white";
-    }
+  return (
+    <div className="hamburger-menu relative z-50">
+      <label className="menu__btn" htmlFor="menu__toggle">
+        <input id="menu__toggle" key="menu__toggle" type="checkbox" onClick={() => setIsChecked(!isChecked)} />
+        <span></span>
+      </label>
 
-    return (
-        <div className="foobar">
-            <span className="open-nav" onClick={openNav}>&#9776; open</span>
-            <div id="mySidenav" className="sidenav">
-                <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
-                <a href="https://opensea.io/collection/kaiju-kingz">OpenSea</a>
-                <a href="https://discord.gg/kaiju-kingz">Discord</a>
-                <a href="https://twitter.com/KaijuKingz">Twitter</a>
-                <a href="#">Lessons</a>
-                <a href="#">Disclaimer</a>
-            </div>
-        </div>
-    )
+      <ul className="menu__box" style={isChecked === true ? { right: "0" } : {}}>
+        <li><a className="menu__item" href="https://opensea.io/collection/kaiju-kingz">OpenSea</a></li>
+        <li><a className="menu__item" href="https://discord.gg/kaiju-kingz">Discord</a></li>
+        <li><a className="menu__item" href="https://twitter.com/KaijuKingz">Twitter</a></li>
+        <li><a className="menu__item" href="#">Lessons</a></li>
+        <li><a className="menu__item" href="#">Disclaimer</a></li>
+      </ul>
+    </div>
+  )
 }
 
 export default SideMenu;
