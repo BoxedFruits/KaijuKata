6014df9711e0a095143bc7b74ff2c56da105f225
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-06-14 18:48:07 -0400

    code blocks with syntax highlighting

---
+++ app/common_components/codeSnippet.tsx
@@ -0,0 +1,35 @@
+import { Prism, Highlight, themes } from "prism-react-renderer"
+
+interface CodeSnippetProps {
+  codeBlock: string
+  language: string
+}
+
+// @ts-ignore
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+require("prismjs/components/prism-solidity");
+
+const CodeSnippet = ({ codeBlock, language }: CodeSnippetProps) => {
+  return (
+    <Highlight
+      theme={themes.shadesOfPurple}
+      code={codeBlock}
+      language={"solidity"}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre style={style}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line })}>
+              <span>{i + 1}</span>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
+
+export default CodeSnippet;
