e3ada2c0d46e734f49709daf6463057e142ee0b2
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-06-23 18:55:17 -0400

    update course viewer

--- app/common_components/course_viewer/courseViewer.tsx
+++ app/common_components/course_viewer/courseViewer.tsx
@@ -1,4 +1,3 @@
-import img from "next/image";
 import LessonList, { ILessonItem } from "./lessonList";
 
 export interface ICourseViewer {
@@ -13,20 +12,23 @@ const CourseViewer = ({ courseName, courseDescription, courseThumbnail, prerequi
   return (
     <>
       <div className="flex grow flex-col h-full md:h-auto md:flex-row bg-gradient-to-r from-zinc-600 to-zinc-800">
-        <div className="left-side md:w-5/12 md:bg-zinc-800 p-5 md:overflow-auto">
-          <h1 className="mb-0 md:mb-7 text-6xl font-bold">{courseName}</h1>
+        <video className="absolute w-full z-0" muted style={{objectFit: "fill", height: "-webkit-fill-available; height: -moz-available; height: fill-available"}}>
+          <source src="/sorashima.mp4" type="video/mp4" />
+        </video>
+        <div className="left-side md:w-5/12 p-8 z-10 md:overflow-auto bg-zinc-900/80">
+          <h1 className="mb-0 md:mb-5 md:mt-16 md:ml-6 text-4xl font-semibold">{courseName}</h1>
           <div className="hidden md:block">
             <LessonList lessons={lessons} />
           </div>
         </div>
-        <div className="right-side md:w-7/12 md:bg-zinc-700 p-7 md:overflow-auto">
-          <div className="course-info">
+        <div className="right-side md:w-7/12 px-8 pt-4 pb-8 z-10 md:bg-zinc-900/95 md:overflow-auto">
+          <div className="course-info p-8">
             <div className="mb-5 flex align-middle items-center justify-center">
               <div className="thumbnail w-50">
-                <img src={courseThumbnail} alt="course thumbnail" style={{objectFit: "cover", width:"100%", height: "100%"}} />
+                <img src={courseThumbnail} alt="course thumbnail" style={{ objectFit: "cover", width: "100%", height: "100%" }} />
               </div>
             </div>
-            <div className="px-5">
+            <div className="px-5 mt-8">
               <h2 className="text-4xl font-semibold">Course Objective</h2>
               <p>{courseDescription}</p>
               {prerequisites &&
--- app/common_components/course_viewer/lessonList.tsx
+++ app/common_components/course_viewer/lessonList.tsx
@@ -14,11 +14,11 @@ interface LessonListProps {
 const LessonList = ({ lessons }: LessonListProps) => {
   return (
     <>
-      <ol className="lesson-list">
+      <ol className="lesson-list px-8 pb-8 pt-0 mx-8 mb-8 mt-6">
         {lessons.map((lesson, index) => {
           return (
             <Link href={lesson.lessonPath} key={index}>
-              <li className="mb-6 bg-zinc-700 p-2 rounded-xl" key={index}>
+              <li className="mb-6 bg-zinc-900 p-2 rounded-xl border-solid border-2 border-orange-700" key={index}>
                 <div className="lesson-item flex items-center">
                   <div className="lesson-thumbnail mr-2">
                     <img src={lesson.thumbnailPath} alt={lesson.lessonName} />
