26904d7e6b32008704d694362fe301e7d29046af
nizzle88 <mathew.edwards90@gmail.com>
2023-07-01 09:54:21 +0200

    Feat: Added SVG for close to the dialogBox

--- app/common_components/dialogBox.tsx
+++ app/common_components/dialogBox.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
 
 interface DialogBoxProps {
 	title: string
@@ -9,34 +10,34 @@ interface DialogBoxProps {
 }
 
 const DialogBox = ({
-	title,
-	description,
-	link,
-	buttonText = 'Start Lesson',
-	closeModal,
+  title,
+  description,
+  link,
+  buttonText = "Start Lesson",
+  closeModal,
 }: DialogBoxProps) => {
-	return (
-		<dialog
-			open
-			className='z-20 rounded-lg max-w-[50%] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] m-0'
-		>
-			<button
-				onClick={closeModal}
-				className='text-gray-400 absolute right-2 top-2'
-			>
-				X
-			</button>
-			<h2 className='font-semibold'>{title}</h2>
-			<p className='pb-6'>{description}</p>
+  return (
+    <dialog
+      open
+      className='z-20 rounded-lg max-w-[50%] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] m-0'
+    >
+      <button
+        onClick={closeModal}
+        className='text-gray-400 absolute right-2 top-2'
+      >
+        <Image src='/close_X.svg' width={20} height={20} />
+      </button>
+      <h2 className='font-semibold'>{title}</h2>
+      <p className='pb-6'>{description}</p>
 
-			<Link
-				href={link}
-				className='rounded-3xl bg-red-400 py-2 px-1 text-sm absolute right-2 bottom-2'
-			>
-				{buttonText}
-			</Link>
-		</dialog>
-	)
-}
+      <Link
+        href={link}
+        className='rounded-3xl bg-red-400 py-2 px-1 text-sm absolute right-2 bottom-2'
+      >
+        {buttonText}
+      </Link>
+    </dialog>
+  );
+};
 
-export default DialogBox
+export default DialogBox;
