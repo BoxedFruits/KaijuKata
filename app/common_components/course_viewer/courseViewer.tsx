0d6d3733291b59e17a5b115b4ed02477b7baad85
nizzle88 <mathew.edwards90@gmail.com>
2023-07-07 15:20:58 +0200

    Chore: Updated the UI to add more consistancy to desktop/mobile views. Added duplicate code to functions

--- app/common_components/course_viewer/courseViewer.tsx
+++ app/common_components/course_viewer/courseViewer.tsx
@@ -15,22 +15,44 @@ const CourseViewer = ({
   prerequisites,
   lessons,
 }: ICourseViewer) => {
+  const displayPreRequisites = (prerequisites?: string[]) => {
+    if (!prerequisites) return null;
+    return (
+      <>
+        <h3 className='mb-1 font-medium text-[15px]'>Prerequisites</h3>
+        {prerequisites.map((prerequisite, index) => {
+          return (
+            <li className='ml-6 font-light text-sm' key={index}>
+              {prerequisite}
+            </li>
+          );
+        })}
+      </>
+    );
+  };
+
+  const displayPill = () => {
+    return (
+      <>
+        <button className='text-sm py-1 px-2 rounded-tl-lg rounded-bl-lg border border-black'>
+					Lesson
+        </button>
+        <button className='text-sm py-1 px-2 rounded-tr-lg rounded-br-lg border border-black'>
+					Course
+        </button>
+      </>
+    );
+  };
+
   return (
     <>
       <div className='flex grow flex-col h-full md:h-auto md:flex-row bg-gradient-to-r from-zinc-600 to-zinc-800'>
         <div className='left-side pt-4 pl-7 md:overflow-auto md:w-5/12 md:bg-zinc-800'>
           <div className='flex items-center justify-between'>
-            <h1 className='mb-0 text-2xl text-black md:mb-7 md:text-6xl md:text-white'>
+            <h1 className='mb-0 text-3xl font-medium md:mb-7 md:text-5xl text-white'>
               {courseName}
             </h1>
-            <div className='pr-7 md:hidden'>
-              <button className='text-sm py-1 px-2 rounded-tl-lg rounded-bl-lg border border-black'>
-								Lesson
-              </button>
-              <button className='text-sm py-1 px-2 rounded-tr-lg rounded-br-lg border border-black'>
-								Course
-              </button>
-            </div>
+            <div className='pr-7 md:hidden'>{displayPill()}</div>
           </div>
 
           <div className='hidden md:block'>
@@ -42,57 +64,36 @@ const CourseViewer = ({
             <div className='mb-5 flex align-middle items-center justify-center'>
               <div className='relative flex'>
                 <div className='hidden z-20 md:block md:absolute top-[-3rem] left-0'>
-                  <button className='text-sm py-1 px-2 rounded-tl-lg rounded-bl-lg border border-black'>
-										Lesson
-                  </button>
-                  <button className='text-sm py-1 px-2 rounded-tr-lg rounded-br-lg border border-black'>
-										Course
-                  </button>
+                  {displayPill()}
                 </div>
+                <div className='absolute top-4 left-4 backdrop-blur-2xl rounded-lg p-2'>
+                  <p className='font-semibold m-0'>
+										Completed:
+                    <span className='font-normal'> No</span>
+                  </p>
+                </div>
+
                 <img
                   src={courseThumbnail}
                   alt='course thumbnail'
                   className='rounded-lg object-cover md:max-w-[50%]'
                 />
                 <div className='hidden md:flex flex-col pl-6'>
-                  <p className='absolute top-[-1.5rem] font-semibold'>
-										Completed:{" "}
-                    <span className='font-normal'>No</span>
-                  </p>
                   <h1 className='text-5xl font-medium'>
                     {courseName}
                   </h1>
-                  {prerequisites && (
-                    <div className='font-medium text-[15px]'>
-                      <h3 className='mb-1'>
-												Prerequisites
-                      </h3>
-                      <ul className='list-disc'>
-                        {prerequisites.map(
-                          (prerequisite, index) => {
-                            return (
-                              <li
-                                className='ml-6 font-normal'
-                                key={index}
-                              >
-                                {prerequisite}
-                              </li>
-                            );
-                          }
-                        )}
-                      </ul>
-                    </div>
-                  )}
+                  <p className='mb-1'>{courseDescription}</p>
+                  <p className='italic text-[10px] absolute bottom-0 m-0'>
+										Contributors: BoxedFruit, Nizzle
+                  </p>
                 </div>
               </div>
             </div>
             {/* MD SCREENS */}
             <div className='hidden md:block'>
-              <h2>Course Summary</h2>
-              <p>{courseDescription}</p>
-              <p className='italic'>
-								Contributors: BoxedFruit, Nizzle
-              </p>
+              <ul className='list-disc'>
+                {displayPreRequisites(prerequisites)}
+              </ul>
             </div>
 
             {/* SM SCREENS */}
@@ -100,35 +101,15 @@ const CourseViewer = ({
               <h2 className='text-3xl font-medium'>
 								Lesson Summary
               </h2>
-              <p>{courseDescription}</p>
-              <p className='font-semibold'>
-								Completed:
-                <span className='font-normal'> No</span>
-              </p>
-              <p className='italic text-sm'>
+              <p className=''>{courseDescription}</p>
+              <p className='italic  text-xs'>
 								Contributors: BoxedFruit, Nizzle
               </p>
-              {prerequisites && (
-                <div>
-                  <h3 className='mb-1 font-medium'>
-										Prerequisites
-                  </h3>
-                  <ul className='list-disc'>
-                    {prerequisites.map(
-                      (prerequisite, index) => {
-                        return (
-                          <li
-                            className='ml-6 font-normal'
-                            key={index}
-                          >
-                            {prerequisite}
-                          </li>
-                        );
-                      }
-                    )}
-                  </ul>
-                </div>
-              )}
+
+              <ul className='list-disc'>
+                {displayPreRequisites(prerequisites)}
+              </ul>
+
               <h3 className='font-medium'>Related Courses</h3>
               <div className='flex mb-4 w-[100%]'>
                 <div>
