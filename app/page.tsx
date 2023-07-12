e089b890ed4a97e18632127c3565ddb6d982edcf
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-07-12 16:26:24 -0400

    fix: fix some styling for bottom half

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
 
   const CourseTile = ({ imagePath, text, link }: ICourseTile) => {
     return (
-      <Link className="relative border-2 rounded-sm border-[#DE2323]" href={link}>
-        <Image className="blur-sm" src={imagePath} height={360} width={360} alt="" />
+      <Link className="relative border-4 rounded-xl border-[#DE2323]" href={link}>
+        <Image className="blur-sm rounded-xl" src={imagePath} height={360} width={360} alt="" />
         <div className="absolute inset-0 flex items-center justify-center">
           <h1 className={`${bungee.className} text-2xl`}>{text}</h1>
         </div>
@@ -30,9 +30,7 @@ export default function Home() {
         <Image className="absolute z-0 bottom-0 left-1/3" src="/terminal.gif" alt="terminal" width={1889} height={564}></Image>
         <div className="flex absolute w-full h-full top-0" style={{ background: "radial-gradient(135.39% 200.08% at -5.91% -9.01%, #000 53.07%, rgba(19, 19, 19, 0.44) 100%)" }}></div>
         <div className="z-10 md:mt-20">
-          <div className={`${bowlby_one_sc.className} mb-12`}>
-            <h1 className="tracking-widest text-5xl md:text-9xl text-[#DE2323]">Kaiju Kata</h1>
-          </div>
+          <h1 className={`${bowlby_one_sc.className} mb-12 tracking-widest text-5xl md:text-9xl text-[#DE2323]`}>Kaiju Kata</h1>
           <div className={bungee.className}>
             <div>
               <h2 className="mb-1 text-sm md:text-2xl">Become a shadowy super-coder.</h2>
@@ -47,10 +45,11 @@ export default function Home() {
       </div>
       <div className="h-64 w-full -top-48 relative" style={{ background: "linear-gradient(#00000078 50%,#0000009e 50%,#000000)" }}></div>
       <div className="h-screen w-full">
+        <div className={`${bowlby_one_sc.className} text-5xl flex justify-center mb-16`}>Courses</div>
         <div className="flex flex-wrap gap-32 m-16 justify-center">
-          <CourseTile imagePath="/shadowy_super_coder.jpg" text="Solidity" link=""></CourseTile>
-          <CourseTile imagePath="/snakeguy.jpg" text="Vyper" link=""></CourseTile>
-          <CourseTile imagePath="/egyptian_bird.jpg" text="Cairo" link=""></CourseTile>
+          <CourseTile imagePath="/shadowy_super_coder.jpg" text="Solidity" link="solidity_lessons"></CourseTile>
+          <CourseTile imagePath="/snakeguy.jpg" text="Vyper" link="vyper_lessons"></CourseTile>
+          <CourseTile imagePath="/egyptian_bird.jpg" text="Cairo" link="cairo_lessons"></CourseTile>
         </div>
       </div>
     </main>
