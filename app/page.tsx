d6fde6a03b47c35813434d5567ceeff65da4a27e
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-07-12 22:32:03 -0400

    fix: make transition of backgrounds more seamless

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
     <main>
       <div className="flex min-h-screen flex-col items-center justify-between p-24" style={{ height: "100vh" }}>
         <Image className="absolute z-0 bottom-0 left-1/3" src="/terminal.gif" alt="terminal" width={1889} height={564}></Image>
-        <div className="flex absolute w-full h-full top-0" style={{ background: "radial-gradient(135.39% 200.08% at -5.91% -9.01%, #000 53.07%, rgba(19, 19, 19, 0.44) 100%)" }}></div>
+        <div className="flex absolute w-full h-full top-0" style={{ background: "linear-gradient(#10101000 50%,#00000014 10%,#000000)" }}></div>
         <div className="z-10 md:mt-20">
           <h1 className={`${bowlby_one_sc.className} mb-12 tracking-widest text-5xl md:text-9xl text-[#DE2323]`}>Kaiju Kata</h1>
           <div className={bungee.className}>
@@ -43,8 +43,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="h-64 w-full -top-48 relative" style={{ background: "linear-gradient(#00000078 50%,#0000009e 50%,#000000)" }}></div>
-      <div className="h-screen w-full">
+      <div className="h-5/6 w-full mt-44">
         <div className={`${bowlby_one_sc.className} text-5xl flex justify-center mb-16`}>Courses</div>
         <div className="flex flex-wrap gap-32 m-16 justify-center">
           <CourseTile imagePath="/shadowy_super_coder.jpg" text="Solidity" link="solidity_lessons"></CourseTile>
