ee2cc28f02af1f540cfb0484263de75d125a1e63
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-07-12 22:21:06 -0400

    feat: add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`overflow-x-hidden ${inter.className}`}>{children}</body>
+      <body className={`overflow-x-hidden ${inter.className}`}>
+        {children}
+        <Image src="/scientists.png" width={1400} height={400} alt="scientists"></Image>
+        <div className="w-full py-4 px-12 rounded-none border-t-[1px] border-light-border bg-light-bg mt-auto">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center">
+              <p className="text-xs lg:text-[14px]">
+                Not affiliated with KaijuKingz or Augminted Labs
+              </p>
+            </div>
+            <div className="flex flex-wrap justify-evenly text-xs lg:text-[14px]">
+              <a href="https://dashboard.kaijukingz.io/" target="_blank" rel="noopener noreferrer" className="border-b-[1px] border-light-border ml-6">Dashboard</a>
+              <a href="https://medium.com/@AugmintedLabs/welcome-to-kaijumart-5ce56ed55761" target="_blank" rel="noopener noreferrer" className="border-b-[1px] border-light-border ml-6">Medium</a>
+              <a href="https://kaijukingz.io/" target="_blank" rel="noopener noreferrer" className="border-b-[1px] border-light-border ml-6">Main Site</a>
+              <a href="https://twitter.com/KaijuKingz" target="_blank" rel="noopener noreferrer" className="border-b-[1px] border-light-border ml-6">Twitter</a>
+              <a href="https://discord.gg/kaiju-kingz" target="_blank" rel="noopener noreferrer" className="border-b-[1px] border-light-border ml-6">Discord</a>
+              <a href="https://opensea.io/collection/kaiju-kingz" target="_blank" rel="noopener noreferrer" className="border-b-[1px] border-light-border ml-6">Opensea</a>
+            </div>
+          </div>
+        </div>
+      </body>
+
     </html>
   );
 }
