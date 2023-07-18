d6e3c802db52534eb518ef22c4f59741d3e8cba0
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-07-17 21:17:02 -0400

    fix: refactor to remove extra div. uses tailwind classes instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode
 }) {
   return (
     <html lang='en'>
@@ -28,110 +28,62 @@ export default function RootLayout({
           <div className='flex justify-between items-center'>
             <div className='flex items-center'>
               <p className='text-xs lg:text-[14px] mb-0'>
-								Not affiliated with KaijuKingz or Augminted Labs
+                Not affiliated with KaijuKingz or Augminted Labs
               </p>
             </div>
-            <div className='flex flex-col text-sm md:hidden'>
-              <a
-                href='https://dashboard.kaijukingz.io/'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-6'
-              >
-								Dashboard
-              </a>
-              <a
-                href='https://medium.com/@AugmintedLabs/welcome-to-kaijumart-5ce56ed55761'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-6'
-              >
-								Medium
-              </a>
-              <a
-                href='https://kaijukingz.io/'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-6'
-              >
-								Main Site
-              </a>
-            </div>
-            <div className='flex flex-col text-sm md:hidden'>
-              <a
-                href='https://twitter.com/KaijuKingz'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-6'
-              >
-								Twitter
-              </a>
-              <a
-                href='https://discord.gg/kaiju-kingz'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-6'
-              >
-								Discord
-              </a>
-              <a
-                href='https://opensea.io/collection/kaiju-kingz'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-6'
-              >
-								Opensea
-              </a>
-            </div>
-            <div className='hidden md:flex flex-wrap justify-evenly text-xs lg:text-[14px]'>
-              <a
-                href='https://dashboard.kaijukingz.io/'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-5'
-              >
-								Dashboard
-              </a>
-              <a
-                href='https://medium.com/@AugmintedLabs/welcome-to-kaijumart-5ce56ed55761'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-5'
-              >
-								Medium
-              </a>
-              <a
-                href='https://kaijukingz.io/'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-5'
-              >
-								Main Site
-              </a>
-              <a
-                href='https://twitter.com/KaijuKingz'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-5'
-              >
-								Twitter
-              </a>
-              <a
-                href='https://discord.gg/kaiju-kingz'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-5'
-              >
-								Discord
-              </a>
-              <a
-                href='https://opensea.io/collection/kaiju-kingz'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='border-b-[1px] border-light-border ml-5'
-              >
-								Opensea
-              </a>
+            <div className='flex flex-row md:flex-wrap md:justify-evenly text-xs lg-text[14px]'>
+              <div className='flex flex-col md:flex-row'>
+                <a
+                  href='https://dashboard.kaijukingz.io/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='border-b-[1px] border-light-border ml-6'
+                >
+                  Dashboard
+                </a>
+                <a
+                  href='https://medium.com/@AugmintedLabs/welcome-to-kaijumart-5ce56ed55761'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='border-b-[1px] border-light-border ml-6'
+                >
+                  Medium
+                </a>
+                <a
+                  href='https://kaijukingz.io/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='border-b-[1px] border-light-border ml-6'
+                >
+                  Main Site
+                </a>
+              </div>
+              <div className='flex flex-col md:flex-row'>
+                <a
+                  href='https://twitter.com/KaijuKingz'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='border-b-[1px] border-light-border ml-6'
+                >
+                  Twitter
+                </a>
+                <a
+                  href='https://discord.gg/kaiju-kingz'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='border-b-[1px] border-light-border ml-6'
+                >
+                  Discord
+                </a>
+                <a
+                  href='https://opensea.io/collection/kaiju-kingz'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='border-b-[1px] border-light-border ml-6'
+                >
+                  Opensea
+                </a>
+              </div>
             </div>
           </div>
         </div>
